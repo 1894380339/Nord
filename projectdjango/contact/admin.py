537f206f52b78fa3912f26ac76c2f342aa375ccb
1894380339 <17520528@gm.uit.edu.vn>
2022-04-15 18:42:42 +0700

    commit2

--- projectdjango/contact/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import md_contact
-# Register your models here.
-admin.site.register(md_contact)
--- projectdjango/contact/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-app_name = 'contact'
-urlpatterns = [
-    path('', views.class_contact.as_view(), name="view_contact"),
-]
