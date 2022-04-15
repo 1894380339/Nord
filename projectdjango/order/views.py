b67c3b02ca8add331342f8dc5e11e4941a97f284
1894380339 <17520528@gm.uit.edu.vn>
2022-04-15 18:24:45 +0700

    delete

--- projectdjango/order/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from cart.models import Cart
-from .models import Order
-from django.views import View
-# Create your views here.
--- projectdjango/user/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .views import View
-
-def view_user(request):
-    if request.user.is_authenticated:
-        customer = request.user
-        username = customer.username
-        return render(request, 'homepage/cart.html',{"user":customer,"username":username})
-    
