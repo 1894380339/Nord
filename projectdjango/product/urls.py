a9e1d8757913dddbd178d509f9168c2995e51a30
1894380339 <17520528@gm.uit.edu.vn>
2022-04-15 18:35:33 +0700

    commit2

--- projectdjango/product/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-app_name = 'product'
-urlpatterns = [
-    path('', views.productView, name="url_products"),
-    path('categary', views.category.as_view(), name="url_category"),
-    path('<int:id>', views.product_detail, name="url_product_detail"),
-    
-]
--- projectdjango/user/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-app_name = 'user'
-urlpatterns = [
-    path('', views.view_user.as_view(), name="url_user"),
-]
